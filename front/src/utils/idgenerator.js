91a3d8cbcd5ea7e7957a1b2a4c0ff4f7f3372089
Sami Tabet <sami.tabet@student.ecp.fr>
2017-06-26 11:05:05 +0200

    Tools for generating ids

---
+++ front/src/utils/idgenerator.js
@@ -0,0 +1,15 @@
+// A simple Id generator
+export const generateId = () => {
+	let date = new Date().getTime();
+	const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+		const r = ((date + Math.random() * 16) % 16) | 0;
+		console.log(r);
+		date = Math.floor(date / 16);
+		return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+	});
+	return uuid;
+};
+s
+// Regex for a uuid
+
+export const idRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
