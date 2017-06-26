6f4ef0c69e004bb3404431ad2a485558ab0fd8a8
Sami Tabet <sami.tabet@student.ecp.fr>
2017-06-26 12:51:40 +0200

    Better HomePage

--- front/src/home.page.js
+++ front/src/home.page.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Container } from './styles/container.style';
+import { Button, Header, Segment } from 'semantic-ui-react';
 
 import { generateId } from './utils/idgenerator';
 
 export default () => (
-	<div>
-		Home Page !
+	<Container>
+	<Segment padded='very' textAlign='center'>
+		<Header as='h1'>Welcome in CodePad ! </Header>
+		<Header as='h3'> 
+			CodePad is a Tool to make small development projects in group easier !
+			To get started just hit the button below
+		</Header>
 		<Link to={`/${generateId()}`}>
-			Get started
+			<Button 
+				size='massive' 
+				primary 
+				content='Start hacking !'
+				icon='write'
+			/>
 		</Link>
-	</div>
+		</Segment>
+	</Container>
 );
