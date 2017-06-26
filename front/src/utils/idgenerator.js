49266c1c3a5641eafa4eef95fd742027628a2e34
Sami Tabet <sami.tabet@student.ecp.fr>
2017-06-26 11:06:56 +0200

    Failure

--- front/src/utils/idgenerator.js
+++ front/src/utils/idgenerator.js
@@ -9,7 +9,7 @@ export const generateId = () => {
 	});
 	return uuid;
 };
-s
+
 // Regex for a uuid
 
 export const idRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
